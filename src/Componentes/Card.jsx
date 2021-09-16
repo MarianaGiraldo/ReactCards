dd9b9afff3afafd755fc54b8a21945aff74269b6
unknown <SENA@MOSAPRCBAFSD058.FormacionSena.RED>
2021-09-16 10:43:31 -0500

    Proyecto React

---
+++ src/Componentes/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.css'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <img src="https://cdn.svgporn.com/logos/python.svg" alt="lang.sgv"  />
+            <h3>Lenguaje</h3>
+        </div>
+    )
+}
+
+export default Card
